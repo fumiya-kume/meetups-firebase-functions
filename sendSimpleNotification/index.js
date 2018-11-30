d400f1fea196b92c6378e351d3211184c36030ca
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-30 19:50:46 +0900

    通知を送れるようにした

--- sendSimpleNotification/index.js
+++ sendSimpleNotification/index.js
@@ -1,6 +1,30 @@
+const firebaseAdmin = require('firebase-admin')
+
 module.exports = async function (context, req) {
-    context.res = {
-        body: "Hello Functions"
+    const serviceAccount = require("../sercret/service_account.json")
+    firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(serviceAccount),
+        databaseURL: "https://meetups-kuxu.firebaseio.com"
+    })
+
+    const topicName = "all"
+    const message = {
+        notification: {
+            title: "サーバーから通知送れるか試してみる",
+            body: "できたら表示されてる！"
+        },
+        topic: topicName
     }
-    context.done();
+
+    await firebaseAdmin.messaging().send(message)
+        .then((response) => {
+            context.res = {
+                body: response
+            }
+        })
+        .catch((error) => {
+            context.res = {
+                body: error
+            }
+        })
 };
